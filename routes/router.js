175f0912181e089dfbb69a8294f60f99f13b691d
Robin-Wynn <wynnrobin95@gmail.com>
2025-11-03 11:09:42 -0600

    create api endpoint and error handling

---
+++ routes/router.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+// Root Route => http://localhost:3000/api
+router.get('/api', (req, res)=> {
+    // res.send('album api')
+    res.json({
+        'All Albums': `http://localhost:${PORT}/api/album`
+    })
+})
+
+// Error Handling
+router.use((req, res, next)=> {
+    res.status(404)
+    .send('<h1>404 Error this page does not exist</h1>')
+})
+
+// router.use((req, res) => {
+//     res.status(404).render('404', {
+//         title: '404 Not Found',
+//         message: `Oops! The page "${req.originalUrl}" doesn't exist.`
+//     })
+// })
+
+module.exports = router
