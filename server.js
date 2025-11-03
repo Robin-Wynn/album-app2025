20b9c15ba3573b88eb481ca2dd297d48de1c9fc9
Robin-Wynn <wynnrobin95@gmail.com>
2025-11-03 10:48:57 -0600

    added helmet and cors to server.js; built config/dbconfig.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+// build server
 const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 3000 
+
+// handle security
+const helmet = require('helmet')
+const cors = required('cors')
+
+// configuring helmet
+// server.use(helmet())
+server.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    directives: {
+        "img-src": [" 'self ' ", "https: data"],
+        "scriptSrc": [" ' self ' ", "cdn.jsdelivr.net"]
+    }
+}))
+
+server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+
+server.listen(PORT, ()=> console.log(`PORTY OVER HERE (～￣▽￣)～ `))
